52b2c68d07c24411e484ea62ebb52387207e22a8
Ali Akbar <mail.septiandri@gmail.com>
2016-07-31 16:04:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# KBBI IV CLI
+
+Didasarkan pada laman http://kbbi4.portalbahasa.com/
+
+## Instalasi
+
+Salin *repository* ini dengan perintah:
+
+    git clone https://github.com/aliakbars/kbbi.git
+
+Jalankan perintah berikut dari folder utama untuk menginstalasi:
+
+    npm install
+
+Jalankan program dengan perintah:
+
+    node index.js
+
+## Daftar pekerjaan
+
+- Daftar turunan kata
+- Perubahan warna untuk .sample
+- Pencarian kata gabungan
---
+++ index.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+var url = require('url');
+var chalk = require('chalk');
+var inquirer = require('inquirer');
+var axios = require('axios');
+var cheerio = require('cheerio');
+
+var baseUrl = 'http://kbbi4.portalbahasa.com';
+var arti = [];
+var kataBerimbuhan = [];
+var gabunganKata = [];
+
+console.log(chalk.bold('Selamat datang di KBBI IV Daring!'));
+
+var questions = [
+    {
+        type: 'input',
+        name: 'entri',
+        message : 'Kata yang ingin Anda cari:'
+    }
+];
+
+inquirer.prompt(questions).then(function (answers) {
+    axios.get(baseUrl + '/entri/' + escape(answers.entri)).then(function (response) {
+        var $ = cheerio.load(response.data);
+        if ($('.syllable').length) {
+            $('.syllable').each(function (i, elm) {
+                console.log(chalk.green($(elm).text()));
+                $(elm).parent().next().children().each(function (i, elm) {
+                    $(elm).find('a.attribute').each(function (i, elm) {
+                        process.stdout.write(chalk.red($(elm).text() + ' '));
+                    });
+
+                    process.stdout.write($(elm).clone()
+                        .find('a.attribute').remove().end()
+                        .find('.latin').remove().end()
+                        .text().trim());
+
+                    if ($(elm).find('.latin').length != 0) {
+                        process.stdout.write(' ' + chalk.green.italic($(elm).find('.latin').text()));
+                    }
+
+                    process.stdout.write('\n');
+                });
+            });
+        } else {
+            console.log(chalk.red('Entri tidak ditemukan.'));
+        }
+    });
+});
